3e16f84890ba949f20953ecdc5fe22a1260cf041
kitty-hub <62388838+kitty-hub@users.noreply.github.com>
2020-04-17 10:36:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,3 @@
-controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
-    Narrator.x += 20
-    Narrator.startEffect(effects.spray)
-    You = sprites.create(img`
-. . . . . f f f f . . . . . 
-. . . f f 5 5 5 5 f f . . . 
-. . f 5 5 5 5 5 5 5 5 f . . 
-. f 5 5 5 5 5 5 5 5 5 5 f . 
-. f 5 5 5 d b b d 5 5 5 f . 
-f 5 5 5 b 4 4 4 4 b 5 5 5 f 
-f 5 5 c c 4 4 4 4 c c 5 5 f 
-f b b f b f 4 4 f b f b b f 
-f b b 4 1 f d d f 1 4 b b f 
-. f b f d 3 d d 3 d f b f . 
-. f e f e 3 3 3 3 e f e f . 
-. e 4 f 6 9 9 9 9 6 f 4 e . 
-. 4 d c 9 9 9 9 9 9 c d 4 . 
-. 4 f b 3 b 3 b 3 b b f 4 . 
-. . f f 3 b 3 b 3 3 f f . . 
-. . . . f f b b f f . . . . 
-`, SpriteKind.Player)
-    You.x += -60
-    Narrator.say("That over there is you. You have bumped your head and have no idea who u r or what you are doing here")
-})
-let You: Sprite = null
-let Narrator: Sprite = null
 scene.setBackgroundImage(img`
 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
@@ -146,22 +120,7 @@ scene.setBackgroundImage(img`
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
 `)
-Narrator = sprites.create(img`
-. . . . . . 5 . 5 . . . . . . . 
-. . . . . f 5 5 5 f f . . . . . 
-. . . . f 1 5 2 5 1 6 f . . . . 
-. . . f 1 6 6 6 6 6 1 6 f . . . 
-. . . f 6 6 f f f f 6 1 f . . . 
-. . . f 6 f f d d f f 6 f . . . 
-. . f 6 f d f d d f d f 6 f . . 
-. . f 6 f d 3 d d 3 d f 6 f . . 
-. . f 6 6 f d d d d f 6 6 f . . 
-. f 6 6 f 3 f f f f 3 f 6 6 f . 
-. . f f d 3 5 3 3 5 3 3 f f . . 
-. . f d f f 3 5 5 3 f d f . . . 
-. . . f f 3 3 3 3 3 f d f . . . 
-. . . f 3 3 5 3 3 5 3 f f . . . 
-. . . f f f f f f f f f . . . . 
-. . . . . f f . . . . . . . . . 
-`, SpriteKind.Player)
-Narrator.say("Hi i am Isabelle the narrator  and i will follow you on your journey and awkwardly tell you what is happening from time to time for example..")
+game.showLongText("Hello Welcome to escape the castle! Where your choices effect your fate", DialogLayout.Bottom)
+game.showLongText("I am the narrator and will tell you what is happening..", DialogLayout.Bottom)
+game.showLongText("Now lets get started", DialogLayout.Bottom)
+game.showLongText("", DialogLayout.Bottom)
